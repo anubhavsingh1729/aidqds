97bc869a635955223a4f45654d32e93e23e70031
anubhavsingh1729 <anubhav.singh1729@gmail.com>
2026-03-10 20:01:16 +0530

    connected to duckdb and create a view from parquet files

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -1,3 +1,15 @@
 import pandas as pd
 import numpy as np
+import duckdb
 
+con = duckdb.connect("../data/aidqds.db")  #connect to duckdb database, if it doesn't exist it will be created
+
+#create a view in duckdb to read all parquet files in the data/raw directory
+con.execute("""
+create or replace view raw_trips AS
+            SELECT * FROM read_parquet('../data/raw/*.parquet')
+""")
+
+
+res = con.execute('select * from raw_trips').df()
+print(res.head())
