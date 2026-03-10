589796d1192f9b6f952d84fbe1b68d81c409130b
anubhavsingh1729 <anubhav.singh1729@gmail.com>
2026-03-10 20:42:38 +0530

    created a table of trips for 6 month data for baseline queries in duckdb

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -6,10 +6,27 @@ con = duckdb.connect("../data/aidqds.db")  #connect to duckdb database, if it do
 
 #create a view in duckdb to read all parquet files in the data/raw directory
 con.execute("""
-create or replace view raw_trips AS
+create view if not exists raw_trips AS
             SELECT * FROM read_parquet('../data/raw/*.parquet')
 """)
 
+#create a table in duckdb with selected columns from raw_trips. This will be our baseline query table.
+con.execute("""
+create table if not exists processed_trips AS
+        SELECT 
+            tpep_pickup_datetime,
+            DATE_TRUNC('hour',tpep_pickup_datetime) AS pickup_hour,   
+            DAYOFWEEK(tpep_pickup_datetime) AS day_of_week,
+            HOUR(tpep_pickup_datetime) AS hour_of_day,
+            PULocationID,
+            DOLocationID,
+            fare_amount,
+            trip_distance,
+            passenger_count,
+            payment_type
+        from raw_trips
+        where trip_distance >0
+""")
 
-res = con.execute('select * from raw_trips').df()
-print(res.head())
+
+print(con.execute("""Select * from processed_trips limit 10""").df())
